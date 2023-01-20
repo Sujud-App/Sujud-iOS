6941c115593cb293750fee97244268085d30a4ab
Babyyoda777 <azlan777@icloud.com>
2023-01-20 18:35:58 +0000

    Initial app commit

---
+++ Salaam/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  Salaam
+//
+//  Created by Muhammad Shah on 20/01/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+
---
+++ Salaam/SalaamApp.swift
@@ -0,0 +1,17 @@
+//
+//  SalaamApp.swift
+//  Salaam
+//
+//  Created by Muhammad Shah on 20/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct SalaamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
